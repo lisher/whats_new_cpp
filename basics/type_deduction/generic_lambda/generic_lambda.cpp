5cda8c1213684a3dca59be2e44421385cafdfb48
lisher <lisher.bass@gmail.com>
2017-03-23 21:20:00 +0100

    Added example for generic lambda

---
+++ basics/type_deduction/generic_lambda/generic_lambda.cpp
@@ -0,0 +1,36 @@
+/*
+ *
+ * What's new in C++
+ * Type deduction - generic lambda
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
+
+int main()
+{
+  std::vector<int> fib {1, 1, 2, 3, 5, 8, 13, 21, 34};
+
+  std::for_each(fib.begin(), fib.end(), [] (int value) { std::cout << "(" << value << ") "; });
+  std::cout << std::endl;
+
+  std::list<std::string> os {"Mint", "Debian", "Fedora", "RedHat", "Ubuntu"};
+
+  std::for_each(os.begin(), os.end(), [] (std::string value) { std::cout << "(" << value << ") "; });
+  std::cout << std::endl;
+
+  auto decorator = [] (const auto & value) { std::cout << "(" << value << ") "; };
+
+  std::set<double> points {1.1, 2.2, 3.3, 4.4};
+
+  std::for_each(points.begin(), points.end(), decorator);
+  std::cout << std::endl;
+  std::for_each(fib.begin(), fib.end(), decorator);
+  std::cout << std::endl;
+
+  return 0;
+}
